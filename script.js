fc46b8eddb89b1a216d031bf53816cf8d91761ec
ademolabakare <106655720+ademolabakare@users.noreply.github.com>
2022-09-20 01:25:37 -0700

    Update script.js
    Signed-off-by: ademolabakare <106655720+ademolabakare@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ $(".white").hover(function(){
 	$(this).removeClass("newBlue");
 });
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-};
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 
 
